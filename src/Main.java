4f6389bb586a2096519b93643d55558fa56d9781
razegod1337 <128294496+razegod1337@users.noreply.github.com>
2023-08-04 21:02:09 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double HeightMetres = 1.87;
-        double WeightKillograms = 98;
-        double bmi = service.calculate(HeightMetres, WeightKillograms);
+        double heightMetres = 1.87;
+        double weightKillograms = 98;
+        double bmi = service.calculate(heightMetres, weightKillograms);
         System.out.println(bmi);
     }
 }
